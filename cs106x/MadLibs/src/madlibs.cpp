9e05038f12660e46e90140f674f9190529c1b482
Keeevini <kevin.ni2000@gmail.com>
2021-01-14 00:24:24 -0800

    Added in cs106x, CS106 init lab, and MadLibs

---
+++ cs106x/MadLibs/src/madlibs.cpp
@@ -0,0 +1,13 @@
+// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+int main() {
+    // TODO: Finish the program!
+
+    cout << "Hello, world!" << endl;   // delete me
+    cout << "Hey this is a test" << endl;
+    return 0;
+}
