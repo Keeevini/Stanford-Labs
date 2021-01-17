d5f7e8d9d1591de61ff57055189c6b500fbbb173
Keeevini <kevin.ni2000@gmail.com>
2021-01-16 19:58:20 -0800

    Now working for < and >

--- cs106x/MadLibs/src/madlibs.cpp
+++ cs106x/MadLibs/src/madlibs.cpp
@@ -13,6 +13,7 @@ using namespace std;
 string introduction();
 string fillIn(string);
 string requestWord(string);
+bool onlyCarrot(string);
 
 int main() {
     // TODO: Finish the program!
@@ -42,34 +43,47 @@ string fillIn(string inputFile) {
 
     output << "Your Mad Lib story: " << endl;
 
+    //parse every line
     while (getline(input, line)) {
         istringstream input2(line); string word;
         string storage; bool toStore = false;
 
-        while(input2 >> word){
-            // cout << word << endl;
-            if(stringContains(word, '<') && stringContains(word, '>')) {
-                word = requestWord(word);
-            }
-            else {
-                if(stringContains(word, '<')) {
-                    toStore = true;
-                    storage = word;
-                } else {
-                    if(toStore)
-                        storage = storage + " " + word;
+        //parse every word
+        while(input2 >> word){            
+
+            if(!onlyCarrot(word)) {
+
+                //check if single <placeholder>
+                if(stringContains(word, '<') && stringContains(word, '>')) {
+                    word = requestWord(word);
                 }
-                if(stringContains(word, '>')) {
-                    word = requestWord(storage);
-                    toStore = false;
-                    storage = "";
+                else {
+
+                    //check if < starting a placeholder
+                    if(stringContains(word, '<')) {
+                        toStore = true;
+                        storage = word;
+                    } else {
+
+                        //add if currently in a placeholder
+                        if(toStore)
+                            storage = storage + " " + word;
+                    }
+
+                    //check if > ending a placeholder
+                    if(stringContains(word, '>')) {
+                        word = requestWord(storage);
+                        toStore = false;
+                        storage = "";
+                    }
                 }
             }
+
+            //output the word
             if(!toStore) {
                 output << word << " ";
             }
         }
-        // cout << line << endl;
         output << endl;
     }
     input.close();
@@ -78,12 +92,22 @@ string fillIn(string inputFile) {
 }
 
 string requestWord(string input) {
+    //init variables
     int openC = stringIndexOf(input, "<");
     int closeC = stringLastIndexOf(input, ">");
     string tag = input.substr(openC+1, closeC-1-openC);
 
+    //check for vowels
     if(tag.at(0) == 'a' || tag.at(0) == 'e' || tag.at(0) == 'i' || tag.at(0) == 'o' || tag.at(0) == 'u') {
         return input.substr(0, stringIndexOf(input, tag.at(0))-1) + getLine("Please type an " + tag + ": ") + input.substr(closeC+1);
     }
         return input.substr(0, stringIndexOf(input, tag.at(0))-1) + getLine("Please type a " + tag + ": ") + input.substr(closeC+1);
+}
+
+bool onlyCarrot(string input) {
+    for(char c : input) {
+        if(c != '<' && c != '>')
+            return false;
+    }
+    return true;
 }
